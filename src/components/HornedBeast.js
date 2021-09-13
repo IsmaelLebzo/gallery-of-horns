dbc1f107177d30cb0ecd65603e15e067c9aaeae3
IsmaelLebzo <20017635@student.ltuc.com>
2021-09-13 16:02:46 +0300

    finished lab02

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,12 +1,39 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 class Hornedbeast extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            numOfVotes: 0
+        }
+    }
+
+    increasNumOfVotes = () => {
+        this.setState({
+            numOfVotes: this.state.numOfVotes + 1
+        })
+    }
+
     render() {
         return (
             <div>
-                <h2>{this.props.title}</h2>
+                {/* <h2>{this.props.title}</h2>
                 <img src={this.props.image_url} alt={this.props.keyword} title={this.props.horns} />
-                <p>{this.props.description}</p>
+                <p>{this.props.description}</p> */}
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={this.props.image_url} alt={this.props.keyword} />
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                            {this.props.description}.<br />Number of Votes {this.state.numOfVotes}
+                        </Card.Text>
+                        <Button onClick={this.increasNumOfVotes} variant="primary">Vote</Button>
+                    </Card.Body>
+                </Card>
             </div>
         )
     }
